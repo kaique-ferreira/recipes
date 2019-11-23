f2710dbd38d9bb95d9c4236ce16b0b0ce9454b68
Aruanã Kaiquê Ferreira <Aruana.Ferreira@externals.adidas.com>
2019-11-23 22:27:27 +0100

    adding up navigation

--- app/src/main/java/com/aruana/recipes/MainActivity.kt
+++ app/src/main/java/com/aruana/recipes/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.aruana.recipes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupActionBarWithNavController(navController = findNavController(R.id.navigationHostFragment))
     }
+
+    override fun onSupportNavigateUp() =
+            findNavController(R.id.navigationHostFragment).navigateUp()
 }
